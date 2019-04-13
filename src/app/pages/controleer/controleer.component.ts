c19ee9851accf83b74cc4a133aac07008c5c2aac
jcvanmulligen <jcvanmulligen@gmail.com>
2019-04-13 16:44:34 +0200

    controleer

--- src/app/pages/controleer/controleer.component.ts
+++ src/app/pages/controleer/controleer.component.ts
@@ -1,9 +1,44 @@
 import { Component } from '@angular/core';
+import {calcPossibleSecurityContexts} from "@angular/compiler/src/template_parser/binding_parser";
 
 @Component({
   templateUrl: './controleer.component.html'
 })
 export class ControleerComponent {
 
+  type = '';
+  nationalityType='';
+  diplomaType='';
+  implications= [];
+
+  redenNationaliteit = '';
+  redenDiploma = '';
+
+  setRequestBadge(event: Event) {
+    this.type = event.target.value;
+
+    }
+
+  setRequestRedenNationaliteit(event: Event) {
+    this.redenNationaliteit = event.target.value;
+
+  }
+  setRequestRedenDiploma(event: Event) {
+    console.log(event.target.value);
+    this.redenDiploma = event.target.value;
+  }
+
+  setImplications(event: Event) {
+    console.log(event.target.value);
+    this.implications.push(event.target.value);
+  }
+
+
+
 
 }
+
+
+
+
+
